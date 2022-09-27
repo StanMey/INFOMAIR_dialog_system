8543ea4958fd77f52fa6527d9b677748c4a0c4c9
IrisFolpmers <iris.folpmers@hotmail.com>
2022-09-27 14:52:19 +0200

    max levenshtein made relative

--- preference_extraction.py
+++ preference_extraction.py
@@ -6,7 +6,7 @@ from typing import List, Union
 
 
 
-def find_preference(options: List[str], user_utterance: str, max_levenshtein: int = 3) -> Union[str, None]:
+def find_preference(options: List[str], user_utterance: str, max_levenshtein: None) -> Union[str, None]:
     """Finds the preference in a sentence out of a list with options and the levensthein distance.
 
     Args:
@@ -31,8 +31,12 @@ def find_preference(options: List[str], user_utterance: str, max_levenshtein: in
             for word in user_utterance.split():
                 distance = Levenshtein.distance(word, option)
                 if distance < smallest_distance:
+                    # if not defined
+                    if not max_levenshtein:
+                        max_levenshtein = len(word)/2
                     smallest_distance = distance
                     best = option
+    # not completely certain
     if best and not current:
         if smallest_distance < max_levenshtein:
             pref = best
