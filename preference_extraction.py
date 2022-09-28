355cd26910bd3b34cac13543a3ebc5e8de282870
StanMey <stan_1998@hotmail.com>
2022-09-28 21:35:45 +0200

    small fix to levensthein so it works with decouple

--- preference_extraction.py
+++ preference_extraction.py
@@ -1,18 +1,17 @@
 import Levenshtein
 import numpy as np
-import re
 
 from typing import List, Union
 
 
 
-def find_preference(options: List[str], user_utterance: str, max_levenshtein: None) -> Union[str, None]:
+def find_preference(options: List[str], user_utterance: str, max_levenshtein: int = 0) -> Union[str, None]:
     """Finds the preference in a sentence out of a list with options and the levenshtein distance.
 
     Args:
         options (List[str]): a list of options for which we search in the text.
         user_utterance (str): The input from the user.
-        max_levenshtein (int, optional): the length of the levenshtein distance. Defaults to None.
+        max_levenshtein (int, optional): the length of the levenshtein distance, if this is 0 than a dynamic levenshtein value is used. Defaults to 0.
 
     Returns:
         Union[str, None]: returns either the best found match or None.
